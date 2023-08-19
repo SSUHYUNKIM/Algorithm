ba745a4a488c8a3e4a833acd0fd4311b04230c1d
SSUHYUNKIM <yellosuhyun@naver.com>
2023-08-19 20:23:48 +0900

    #11382

---
+++ math/solution/11382.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+long long A, B, C;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> A >> B >> C;
+
+    cout << A + B + C << '\n';
+    return 0;
+}
