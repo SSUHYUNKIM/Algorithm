96c10f9673a3578e29eb185d044ad2b33311b5c7
SSUHYUNKIM <yellosuhyun@naver.com>
2023-08-19 20:06:23 +0900

    #10926

---
+++ implementation/solution/10926.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+string input;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> input;
+
+    cout << input << "??!" <<'\n';
+    return 0;
+}
