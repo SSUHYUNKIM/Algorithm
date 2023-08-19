0d942e6c168f3923c198eff19c87de0e18510417
SSUHYUNKIM <yellosuhyun@naver.com>
2023-08-19 21:49:37 +0900

    #문자열 돌리기

--- math/solution/문자열 돌리기.cpp
+++ math/solution/문자열 돌리기.cpp
@@ -3,13 +3,16 @@
 
 using namespace std;
 
-string str1, str2;
+string str;
 
 int main(void) 
 {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> str1 >> str2;
-
-    cout << str1 << str2 << '\n';
+    cin >> str;
+    
+    for(int i = 0; i < str.length(); i++)
+    {
+        cout << str[i] << '\n';
+    }
     return 0;
 }
