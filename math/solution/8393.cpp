9cfd4206f8980d0669f3cab257820fe1f00e3a6d
SSUHYUNKIM <yellosuhyun@naver.com>
2023-08-22 21:21:27 +0900

    #8393

---
+++ math/solution/8393.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int n, result = 0;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        result += i;
+    }
+    cout << result << '\n';
+    return 0;
+}
