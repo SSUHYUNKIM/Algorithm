749c5939ad404fe45870c4817062700d900ef7b8
SSUHYUNKIM <yellosuhyun@naver.com>
2023-08-27 21:58:14 +0900

    #2743

---
+++ implementation/solution/2743.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+string input;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> input;
+    cout << input.size() << '\n';
+    return 0;
+}
