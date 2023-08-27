41344bde4c5137168418380b182dd7b1a7b744e7
SSUHYUNKIM <yellosuhyun@naver.com>
2023-08-27 21:55:29 +0900

    #27866

---
+++ implementation/solution/27866.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+string S;
+int i;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> S >> i;
+    
+    cout << S[i - 1] << '\n';
+    return 0;
+}
