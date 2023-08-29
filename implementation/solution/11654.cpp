4a19287b7652da16dbd5528779a03555fafed58a
SSUHYUNKIM <yellosuhyun@naver.com>
2023-08-29 09:49:49 +0900

    #11654

---
+++ implementation/solution/11654.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+char input;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> input;
+    cout << (int)input << '\n';
+    return 0;
+}
