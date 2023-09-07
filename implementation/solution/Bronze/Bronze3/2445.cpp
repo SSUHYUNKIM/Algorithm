2d547714c9b9972373dd9c5b34afa249d38b4294
SSUHYUNKIM <yellosuhyun@naver.com>
2023-09-07 11:13:24 +0900

    #2445

---
+++ implementation/solution/Bronze/Bronze3/2445.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin >> N;
+
+    for(int i = 1; i <= N; i++)
+    {
+        for(int j = 0; j < i; j++)
+            cout << "*";
+        for(int j = 2 * (N - i); j > 0; j--)
+            cout << " ";
+        for(int j = 0; j < i; j++)
+            cout << "*";
+        cout << '\n';
+    }
+
+    for(int i = N - 1; i > 0; i--)
+    {
+        for(int j = 0; j < i; j++)
+            cout << "*";
+        for(int j = 2 * (N - i); j > 0; j--)
+            cout << " ";
+        for(int j = 0; j < i; j++)
+            cout << "*";
+        cout << '\n';
+    }
+
+    return 0;
+}
