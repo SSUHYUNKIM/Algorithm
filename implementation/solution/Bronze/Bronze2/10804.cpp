2f18fd4362b36659544852e9cb3abf71af041b6a
SSUHYUNKIM <yellosuhyun@naver.com>
2023-09-11 11:54:04 +0900

    #10804

--- implementation/solution/Bronze/Bronze2/10804.cpp
+++ implementation/solution/Bronze/Bronze2/10804.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+
+int a, b;
+int card[21];
 
 int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    for(int i = 1; i < 21; i++)
+        card[i] = i;
+
+    for(int i = 0; i < 10; i++)
+    {
+        cin >> a >> b;
+        reverse(card + a, card + b + 1);
+    }
+
+    for(int i = 1; i < 21; i++)
+        cout << card[i] << " ";
 
     return 0;
 }
